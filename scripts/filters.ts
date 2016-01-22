2a3e7c0181f7af59148c9c47f95d198fcc78eafb
duma <dumaron@gmail.com>
2016-01-22 22:58:04 +0100

    added 2 filters; first steps with ui layouts

--- scripts/filters.ts
+++ scripts/filters.ts
@@ -348,7 +348,97 @@ filters['quadrati 2'] = function (canvas: HTMLCanvasElement): void {
             context.closePath();
 
         }
+};
 
+filters['quadrati 3'] = function (canvas: HTMLCanvasElement): void {
+    let
+        w = canvas.width,
+        h = canvas.height,
+        magicNumber = w / 40,
+        x,
+        y,
+        index,
+        context = canvas.getContext('2d'),
+        img = context.getImageData(0, 0, w, h),
+        pixels = img.data,
+        side;
+
+
+    for (x = 0; x <= w; x += magicNumber)
+        for (y = 0; y <= h; y += magicNumber) {
+            index = getIndex(Math.floor(x), Math.floor(y), w);
+            side = magicNumber * 4 * Math.random();
+            context.beginPath();
+            context.rect(x, y, side, side + side * Math.random());
+            context.fillStyle = pixelToHex(pixels[index], pixels[index+1], pixels[index+2]);
+            context.fill();
+            context.closePath();
+
+        }
+};
+
+filters['quadrati 4'] = function (canvas: HTMLCanvasElement): void {
+    let
+        w = canvas.width,
+        h = canvas.height,
+        magicNumber = w / 80,
+        x,
+        y,
+        i,
+        index,
+        context = canvas.getContext('2d'),
+        img = context.getImageData(0, 0, w, h),
+        pixels = img.data,
+        limit = 30,
+        diffR,
+        diffG,
+        diffB,
+        nextIndex,
+        minNext,
+        avgDiff,
+        side,
+        squares = [],
+        step = 50;
+
+    for (x = 0; x < w; x += magicNumber)
+        for (y = 0; y < h; y += magicNumber) {
+            if (x + step > w)
+                continue;
+
+            index = getIndex(Math.floor(x), Math.floor(y), w);
+            nextIndex = getIndex(Math.floor(x) + step, Math.floor(y), w);
+            diffR = Math.abs(pixels[index] - pixels[nextIndex]);
+            diffG = Math.abs(pixels[index+1] - pixels[nextIndex+1]);
+            diffB = Math.abs(pixels[index+2] - pixels[nextIndex+2]);
+            minNext = Math.min(pixels[nextIndex], pixels[nextIndex+1], pixels[nextIndex+2]);
+            avgDiff = avg([diffB, diffG, diffR]);
+
+
+            if (avgDiff > limit) {
+                squares.push({
+                    x: Math.floor(x),
+                    y: Math.floor(y),
+                    color: pixelToHex(diffR *3, diffG *2, diffB *2)
+                })
+            }
+        }
+
+    for (i=0; i<pixels.length; i+=4) {
+        pixels[i] = pixels[i+1] = pixels[i+2] = pixels[i+3] = 255;
+    }
+
+    console.log(squares);
+
+    context.putImageData(img, 0, 0);
+
+    for(i=0; i<squares.length; i++) {
+        side = magicNumber * 2 * Math.random();
+        context.beginPath();
+        context.rect(squares[i].x, squares[i].y, side, side);
+        context.fillStyle = squares[i].color;
+        context.fill();
+        context.closePath();
+    }
 };
 
 function getFilters() {
